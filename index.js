e0b6ef3a2c897e7afa685cbc6e446ff02d447055
raresNagy <rares.nagy@gmail.com>
2022-03-07 12:32:34 +0200

    pescuit

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
+// Libraries
 var fs = require('fs');
-var words = fs.readFileSync('dict.txt', 'utf-8');
+var readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+var words = fs.readFileSync('5letterwords.txt', 'utf-8');
 words = words.split('\n');
 for (var _i = 0, words_1 = words; _i < words_1.length; _i++) {
     var testWord = words_1[_i];
-    if (testWord.length != 5)
-        continue;
-    fs.appendFile('./5letterwords.txt', testWord + '\n', function (err) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-    });
 }
